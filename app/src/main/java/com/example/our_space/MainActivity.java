6ad78075f58a6d322de9887e8ac86b94640df8b8
alexleroyross <alrss656@hotmail.com>
2019-11-18 07:25:34 -0500

    More messaging functionality

--- app/src/main/java/com/example/our_space/MainActivity.java
+++ app/src/main/java/com/example/our_space/MainActivity.java
@@ -5,40 +5,94 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends BaseActivity {
 
+    private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabase;
+    private final String TAG = "ourDatabase";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        basicWrite();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabase = mFirebaseDatabase.getReference();
+
+        // Read from the database
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 
-    public void basicWrite() {
-        mDatabase.child("TestChild").setValue("This is a test message")
+    public void basicWrite(String key, String value) {
+        mDatabase.child(key).setValue(value)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d("basicWrite","Database call was successful");
+                        Log.d(TAG,"Database call to write was successful");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.e("basicWrite", e.toString());
-                        Log.e("basicWrite","Database call failed");
+                        Log.e(TAG, e.toString());
+                        Log.e(TAG,"Database call to write failed");
                     }
                 });
+    }
+
+    public void basicWrite(Message message) {
+        mDatabase.child(message.room).setValue(message.body)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG,"Database call to write was successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, e.toString());
+                        Log.e(TAG,"Database call to write failed");
+                    }
+                });
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.enterMessage);
+        String messageText = editText.getText().toString();
+//        basicWrite("TestChild", messageText);
+        
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+
+        Message message = new Message(currentFirebaseUser.getUid(), currentFirebaseUser.getDisplayName(), "TestRoom", messageText);
+        basicWrite(message);
     }
 }
