39dd1d0c06227dfb790f233b9eb089b560175fe8
alexleroyross <alrss656@hotmail.com>
2019-11-17 21:54:40 -0500

    I don't know forget this branch

---
+++ app/src/main/java/com/example/our_space/MainActivity_old.java
@@ -0,0 +1,233 @@
+package com.example.our_space;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressLint("MissingPermission")
+public class MainActivity_old extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    public void runtimeEnableAutoInit() {
+        // [START fcm_runtime_enable_auto_init]
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+        // [END fcm_runtime_enable_auto_init]
+    }
+
+    public void deviceGroupUpstream() {
+        // [START fcm_device_group_upstream]
+        String to = "a_unique_key"; // the notification key
+        AtomicInteger msgId = new AtomicInteger();
+        FirebaseMessaging.getInstance().send(new RemoteMessage.Builder(to)
+                .setMessageId(String.valueOf(msgId.get()))
+                .addData("hello", "world")
+                .build());
+        // [END fcm_device_group_upstream]
+    }
+
+    // [START fcm_get_account]
+    public String getAccount() {
+        // This call requires the Android GET_ACCOUNTS permission
+        Account[] accounts = AccountManager.get(this /* activity */).
+                getAccountsByType("com.google");
+        if (accounts.length == 0) {
+            return null;
+        }
+        return accounts[0].name;
+    }
+    // [END fcm_get_account]
+
+    public void getAuthToken() {
+        // [START fcm_get_token]
+        String accountName = getAccount();
+
+        // Initialize the scope using the client ID you got from the Console.
+        final String scope = "audience:server:client_id:"
+                + "1262xxx48712-9qs6n32447mcj9dirtnkyrejt82saa52.apps.googleusercontent.com";
+
+        String idToken = null;
+        try {
+            idToken = GoogleAuthUtil.getToken(this, accountName, scope);
+        } catch (Exception e) {
+            Log.w(TAG, "Exception while getting idToken: " + e);
+        }
+        // [END fcm_get_token]
+    }
+
+    // [START fcm_add_to_group]
+    public String addToGroup(
+            String senderId, String userEmail, String registrationId, String idToken)
+            throws IOException, JSONException {
+        URL url = new URL("https://fcm.googleapis.com/fcm/googlenotification");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+
+        // HTTP request header
+        con.setRequestProperty("project_id", senderId);
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("POST");
+        con.connect();
+
+        // HTTP request
+        JSONObject data = new JSONObject();
+        data.put("operation", "add");
+        data.put("notification_key_name", userEmail);
+        data.put("registration_ids", new JSONArray(Arrays.asList(registrationId)));
+        data.put("id_token", idToken);
+
+        OutputStream os = con.getOutputStream();
+        os.write(data.toString().getBytes("UTF-8"));
+        os.close();
+
+        // Read the response into a string
+        InputStream is = con.getInputStream();
+        String responseString = new Scanner(is, "UTF-8").useDelimiter("\\A").next();
+        is.close();
+
+        // Parse the JSON string and return the notification key
+        JSONObject response = new JSONObject(responseString);
+        return response.getString("notification_key");
+    }
+    // [END fcm_add_to_group]
+
+    public void removeFromGroup(String userEmail, String registrationId, String idToken) throws JSONException {
+        // [START fcm_remove_from_group]
+        // HTTP request
+        JSONObject data = new JSONObject();
+        data.put("operation", "remove");
+        data.put("notification_key_name", userEmail);
+        data.put("registration_ids", new JSONArray(Arrays.asList(registrationId)));
+        data.put("id_token", idToken);
+        // [END fcm_remove_from_group]
+    }
+
+    public void sendUpstream(String message) {
+        final String SENDER_ID = "YOUR_SENDER_ID";
+        final int messageId = 0; // Increment for each
+        // [START fcm_send_upstream]
+        FirebaseMessaging fm = FirebaseMessaging.getInstance();
+        fm.send(new RemoteMessage.Builder(SENDER_ID + "@fcm.googleapis.com")
+                .setMessageId(Integer.toString(messageId))
+                .addData("my_message", message)
+                .addData("my_action","SAY_HELLO")
+                .build());
+        // [END fcm_send_upstream]
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+
+//        Button subscribeButton = findViewById(R.id.subscribeButton);
+//        subscribeButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "Subscribing to weather topic");
+//                // [START subscribe_topics]
+//                FirebaseMessaging.getInstance().subscribeToTopic("weather")
+//                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<Void> task) {
+//                                String msg = getString(R.string.msg_subscribed);
+//                                if (!task.isSuccessful()) {
+//                                    msg = getString(R.string.msg_subscribe_failed);
+//                                }
+//                                Log.d(TAG, msg);
+//                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                // [END subscribe_topics]
+//            }
+//        });
+
+//        Button logTokenButton = findViewById(R.id.logTokenButton);
+//        logTokenButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Get token
+//                // [START retrieve_current_token]
+//                FirebaseInstanceId.getInstance().getInstanceId()
+//                        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+//                                if (!task.isSuccessful()) {
+//                                    Log.w(TAG, "getInstanceId failed", task.getException());
+//                                    return;
+//                                }
+//
+//                                // Get new Instance ID token
+//                                String token = task.getResult().getToken();
+//
+//                                // Log and toast
+//                                String msg = getString(R.string.msg_token_fmt, token);
+//                                Log.d(TAG, msg);
+//                                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//                // [END retrieve_current_token]
+//            }
+//        });
+    }
+
+
+    public void sendMessage(View view) {
+        EditText editText = findViewById(R.id.enterMessage);
+        sendUpstream(editText.getText().toString());
+    }
+}
