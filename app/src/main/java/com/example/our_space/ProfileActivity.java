bd8e89dc93d0bcc494d6bcfed057b8ee4edff833
alexleroyross <alrss656@hotmail.com>
2019-12-11 03:12:39 -0500

    profile page fully works

--- app/src/main/java/com/example/our_space/ProfileActivity.java
+++ app/src/main/java/com/example/our_space/ProfileActivity.java
@@ -10,11 +10,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.io.InputStream;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -23,8 +32,29 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
         new DownloadImageTask((ImageView) findViewById(R.id.imageView))
-                .execute(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString());
+                .execute(firebaseAuth.getCurrentUser().getPhotoUrl().toString());
+        final TextView emailText = (TextView) findViewById(R.id.emailValue);
+        emailText.setText(firebaseAuth.getCurrentUser().getEmail());
+        final TextView totalPosts = (TextView) findViewById(R.id.totalPostsValue);
+//        totalPosts.setText(database.child("users").child(firebaseAuth.getUid())..toString());
+
+        database.child("users").child(firebaseAuth.getUid()).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Get map of users in datasnapshot
+                        totalPosts.setText(Long.toString(dataSnapshot.getChildrenCount()));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //handle databaseError
+                    }
+                });
     }
 
 
