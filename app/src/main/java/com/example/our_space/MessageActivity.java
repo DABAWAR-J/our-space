a693af8c153f6320a7554ee35b528762d8e7fe93
aafricke <33527666+aafricke@users.noreply.github.com>
2019-12-10 21:54:29 -0500

    Added geolocation filtering to drop down list to disabled rooms that are not within range

--- app/src/main/java/com/example/our_space/MessageActivity.java
+++ app/src/main/java/com/example/our_space/MessageActivity.java
@@ -2,7 +2,13 @@ package com.example.our_space;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,13 +41,27 @@ public class MessageActivity extends BaseActivity {
     private EditText messageField;
     private Spinner mySpinner;
     private String currentRoom;
+    private Integer currentPosition=0;
     private ChildEventListener postListener;
+
+    //Location Tracking
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+    private final long MIN_TIME = 500; // 1 second
+    private final long MIN_DIST = 1; // 5 meters
+
+    private double lat; //latitude
+    private double lng; //longitude
+
+    private String latText = ""; //latitude string
+    private String lngText; //longitude string
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        FirebaseApp.initializeApp(this);
+        //FirebaseApp.initializeApp(this);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -56,6 +76,47 @@ public class MessageActivity extends BaseActivity {
         mySpinner.setAdapter(myAdapter);
 
         currentRoom = (String) mySpinner.getSelectedItem();
+
+
+        //Location Tracking
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                lat = location.getLatitude();
+                lng = location.getLongitude();
+
+                latText = String.valueOf(lat);
+                lngText = String.valueOf(lng);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DIST, locationListener);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -74,8 +135,34 @@ public class MessageActivity extends BaseActivity {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                messageField.setEnabled(true);
+                messageField.setText("");
                 String previousRoom = currentRoom;
+                Integer previousInt = currentPosition;
                 currentRoom = (String) parent.getItemAtPosition(position);
+                currentPosition = position;
+                Double roomLat = 0.0;
+                Double roomLng = 0.0;
+                if (currentRoom.equals("DECKER HALL 360")) {
+                    roomLat = 40.11179156;
+                    roomLng = -85.66601368;
+                }
+                if (currentRoom.equals("DECKER HALL 351")) {
+                    roomLat = 40.1116857;
+                    roomLng = -85.66618809;
+                }
+                if (currentRoom.equals("DECKER LOUNGE")) {
+                    roomLat = 40.11163259;
+                    roomLng = 85.66634455;
+                }
+                if(((latText != "") && (lat - roomLat < 0.00005)&& (lng - roomLng < .00005))|| currentRoom.equals("CHOOSE ROOM")) {
+                    if (currentRoom.equals("CHOOSE ROOM")) {
+                        messageField.setEnabled(false);
+                    }
+                }else {
+                    parent.setSelection(previousInt);
+                }
+
 
                 if(!previousRoom.equals(currentRoom)) {
                     LinearLayout linearLayout = findViewById(R.id.messageLinearLayout);
